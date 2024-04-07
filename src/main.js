cd2b643da9fac96a063a583cb1f20eba4fa24c5d
cieme <aslslgood22@163.com>
2024-04-07 17:08:59 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 引入样式
+import "vue-easytable/libs/theme-default/index.css";
+// 引入组件库
+import VueEasytable from "vue-easytable/packages/index";
+
+Vue.use(VueEasytable);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,206 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <ve-table
+      ref="veTable"
+      style="width: 100%"
+      :scroll-width="0"
+      :columns="columns"
+      :table-data="tableData"
+      :border-around="true"
+      :border-x="true"
+      :border-y="true"
+      :rowStyleOption="rowStyleOption"
+      row-key-field-name="rowKey"
+      :clipboard-option="clipboardOption"
+      :columnWidthResizeOption="columnWidthResizeOption"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      copyData: null,
+      clipboardOption: {
+        copy: true,
+        paste: true,
+        cut: true,
+        delete: true,
+        beforeCopy: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          console.log("beforeCopy");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+          this.copyData = data;
+        },
+        afterCopy: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          console.log("afterCopy");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+        },
+        beforePaste: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          data = this.copyData;
+          console.log("beforePaste");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+        },
+        afterPaste: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          data = this.copyData;
+          console.log("afterPaste");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+          console.log(this.tableData);
+        },
+        beforeCut: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          console.log("beforeCut");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+        },
+        afterCut: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          console.log("afterCut");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+        },
+        beforeDelete: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          console.log("beforeDelete");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+        },
+        afterDelete: ({ data, selectionRangeIndexes, selectionRangeKeys }) => {
+          console.log("afterDelete");
+          this.log({ data, selectionRangeIndexes, selectionRangeKeys });
+        },
+      },
+      columnResizeInfo: {
+        column: "",
+        differWidth: "",
+        columnWidth: "",
+        tableWidth: "",
+      },
+      rowStyleOption: {
+        clickHighlight: false,
+        hoverHighlight: false,
+      },
+      columnWidthResizeOption: {
+        // default false
+        enable: true,
+        // column resize min width
+        minWidth: 30,
+        // column size change
+        sizeChange: ({ column, differWidth, columnWidth }) => {
+          this.columnResizeInfo.column = column;
+          this.columnResizeInfo.differWidth = differWidth;
+          this.columnResizeInfo.columnWidth = columnWidth;
+        },
+      },
+      columns: [
+        {
+          key: "index",
+          title: "",
+          width: 100,
+          align: "center",
+          fixed: "left",
+          editable: false,
+          edit: false,
+          operationColumn: true,
+          children: [
+            {
+              field: "index",
+              key: "index",
+              title: "string",
+              width: 100,
+              editable: false,
+              edit: false,
+              operationColumn: true,
+              renderBodyCell: ({ row, column, rowIndex }, h) => {
+                row, column, h;
+                return ++rowIndex;
+              },
+            },
+          ],
+        },
+      ],
+      tableData: [],
+    };
+  },
+  created() {
+    this.initColumns();
+    this.initTableData();
+  },
+  mounted() {
+    this.addEvent();
+  },
+
+  methods: {
+    addEvent() {
+      const handle = (event) => {
+        if (event.keyCode == 8) {
+          const newEvent = new KeyboardEvent("keydown", {
+            key: "Backspace",
+            code: "Backspace",
+            keyCode: 46,
+            which: 46,
+            charCode: 46,
+            cancelable: true,
+            bubbles: true,
+          });
+          document.dispatchEvent(newEvent);
+        }
+      };
+      document.addEventListener("keydown", handle);
+      this.$once("hook:beforeDestroy", () => {
+        document.removeEventListener("keydown", handle);
+      });
+    },
+    log({ data, selectionRangeIndexes, selectionRangeKeys }) {
+      console.log("data::", data);
+      console.log("selectionRangeIndexes::", selectionRangeIndexes);
+      console.log("selectionRangeKeys::", selectionRangeKeys);
+    },
+    initColumns() {
+      for (let i = 0; i < 10; i++) {
+        this.columns.push({
+          key: `${i + 1}`,
+          title: `title_${i + 1}`,
+          edit: true,
+          width: 100,
+          align: "center",
+          children: [
+            {
+              field: `${i + 1}`,
+              key: `${i + 1}`,
+              title: `string`,
+              edit: true,
+              width: 100,
+              renderBodyCell: ({ row, column, rowIndex }, h) => {
+                row, column, rowIndex, h;
+                if (row[`${i + 1}`] instanceof Array) {
+                  return row[`${i + 1}`].map((item) => item.data).join(",");
+                }
+                return row[`${i + 1}`]?.data;
+              },
+            },
+          ],
+        });
+      }
+    },
+    initTableData() {
+      const data = [];
+      for (let i = 0; i < 5; i++) {
+        const obj = {};
+        for (let j = 0; j < 8; j++) {
+          obj[`${j + 1}`] = {
+            data: `${j + 1}_${i + 1}`,
+          };
+        }
+        obj[`9`] = [{ data: "11" }, { data: "22" }, { data: "33" }];
+        obj.rowKey = i;
+        data.push(obj);
+      }
+      this.tableData = data;
+      console.log(data);
+    },
   },
 };
 </script>
+<style>
+body {
+  min-height: 100vh;
+}
+.container {
+  width: 100%;
+}
+</style>
